a21173b50d5cbc6493298d21200e2f20f598ae22
Merouane HARRIGA <harriga_merouane@hotmail.fr>
2019-01-17 21:36:59 +0100

    feat(Bank Account): Added the main app components

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepositComponent,
+    WithdrawComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule
